5e93b0dd1b58f2408def084f59d57df0f5648426
Katie <kebraun02@gmail.com>
2020-07-14 18:08:34 -0500

    second commit

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ let destination = '';
 if (vacationType === 'musical') {
     destination = 'New Orleans';
 } else if (vacationType === 'tropical') {
-    destination = 'a beach in Mexico'
+    destination = 'a beach in Mexico';
 } else if (vacationType === 'adventurous') {
-    destination = 'go whitewater rafting at the Grand Canyon'
+    destination = 'go whitewater rafting at the Grand Canyon';
 } else {
-    alert('Customer must choose either "Musical", "Tropical", or "Adventurous".')
+    alert('Customer must choose either "Musical", "Tropical", or "Adventurous".');
 }
 
 let transportation = '';
@@ -20,7 +20,7 @@ if (groupSize == 1 || groupSize == 2) {
 } else if (groupSize >= 6) {
     transportation = "charter flight";
 } else {
-    alert('Customer must choose a number greater than 0.')
+    alert('Customer must choose a number greater than 0.');
 }
 
 const result = "Since you have a group of " + groupSize + " and would like to go on a " + vacationType + " vacation, you should take a " + transportation + " to " + destination + ".";
